d91348239bde7307e6c1d2c18a7421ad830fc5a8
cwise24 <cwise24@gmail.com>
2021-06-13 12:48:41 -0700

    static css

--- docs/conf.py
+++ docs/conf.py
@@ -77,6 +77,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # ---sphinx-themes-----
 html_theme = 'sphinx_rtd_theme'
 
+
+def setup(app):
+    app.add_css_file('css/static.css')
+
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
